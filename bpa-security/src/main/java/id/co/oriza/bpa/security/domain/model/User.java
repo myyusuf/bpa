0882acec818f4358b11938fa1ceae26ad8b1e43b
Yusuf <myyusuf1911@gmail.com>
2014-11-29 00:38:23 +0700

    bpa-security dependency

--- bpa-security/src/main/java/id/co/oriza/bpa/security/domain/model/User.java
+++ bpa-security/src/main/java/id/co/oriza/bpa/security/domain/model/User.java
@@ -1,5 +1,108 @@
 package id.co.oriza.bpa.security.domain.model;
 
-public class User {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "BPA_USER")
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "user_seq_gen")
+	@SequenceGenerator(name = "user_seq_gen", sequenceName = "USER_ENTITY_SEQ")
+	@Column(name = "ID")
+	private Long id;
+
+	@NotEmpty
+	@Column(name = "USER_NAME")
+	private String username;
+
+	@Column(name = "PASSWORD")
+	private String password;
+
+	@NotEmpty
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+
+	@NotEmpty
+	@Column(name = "LAST_NAME")
+	private String lastName;
+
+	@NotEmpty
+	@Column(name = "EMAIL")
+	private String email;
+
+	@Column(name = "NOTES")
+	private String notes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
 
 }
