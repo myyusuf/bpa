4da709ca59f760cb568eb9e1ef73db03e308c0ac
Yusuf <myyusuf1911@gmail.com>
2014-11-29 05:59:10 +0700

    fix requirejs

---
+++ bpa-security/src/main/webapp/js/app/bpa-security-app.js
@@ -0,0 +1,17 @@
+﻿define(["require", "jQuery", "jqxcore", "jqxtabs", "jqxbuttons", "jqxtree", "jqxpanel", "jqxscrollbar", "jqxexpander", 
+        "jqxsplitter", "jqxmenu", "jqxnavigationbar", 
+        "jqxgrid.pager", "jqxgrid.sort", "jqxgrid.edit", "jqxgrid.selection", "jqxlistbox", "jqxdropdownlist", "jqxgrid", "jqxdata"], function (require) {
+    var initialize = function () {
+    	
+        $(document).ready(function () {
+        	
+            
+            $("#jqxMenu").jqxMenu({ width: '100%', theme: 'metro'});
+            $("#jqxMenu").css('visibility', 'visible');
+            
+        });
+    };
+    return {
+        initialize: initialize
+    };
+});
