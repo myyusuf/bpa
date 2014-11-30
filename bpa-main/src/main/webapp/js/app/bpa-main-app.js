dc4b10875151c1d246b38b5e573186e650f49a95
Yusuf <myyusuf1911@gmail.com>
2014-11-30 19:01:33 +0700

    add workspace_main

---
+++ bpa-main/src/main/webapp/js/app/bpa-main-app.js
@@ -0,0 +1,63 @@
+﻿define(["require", "jQuery", "tinypubsub", "jqxcore", "jqxtabs", "jqxbuttons", "jqxtree", "jqxpanel", "jqxscrollbar", "jqxexpander", 
+        "jqxsplitter", "jqxmenu", "jqxnavigationbar", 
+        "jqxgrid.pager", "jqxgrid.sort", "jqxgrid.edit", "jqxgrid.selection", "jqxlistbox", "jqxdropdownlist", "jqxgrid", "jqxdata"], function (require) {
+    var initialize = function () {
+    	
+        $(document).ready(function () {
+        	
+            
+        	var contentHeight = function(){
+        		return $(document).height()-77;
+        	}
+        	
+        	var feedExpanderHeight = function(){
+        		return $(document).height()-101;
+        	}
+        	
+        	
+        	$('#mainSplitter').jqxSplitter({  width: '100%', height: contentHeight(), panels: [{ size: '15%', min: 100 }, {size: '85%', min: 200}], theme: 'metro' });
+            $('#contentSplitter').jqxSplitter({ width: '100%', height: '100%',  orientation: 'horizontal', 
+            	panels: [{ size: '70%', min: 100, collapsible: false }, { min: 100, collapsible: true}], theme: 'metro' });
+            $('#contentSplitter').css({margin : "0px"})
+        
+            $("#feedExpander").jqxExpander({toggleMode: 'none', showArrow: false, width: "100%", height: feedExpanderHeight(), 
+            	theme: 'arctic',
+            	initContent: function () {
+                }
+            });
+            
+            $("#contentExpander").css({overflow: "hidden", marginTop: "0px", marginBottom: "0px"});
+            
+            $("#jqxNavigationBar").jqxNavigationBar({ width: "100%", height: feedExpanderHeight(), expandMode: "singleFitHeight", theme: 'metro'});
+            $('#jqxNavigationBar').css({marginLeft: "1px"});
+            $('#jqxTree').jqxTree({ height: '100%', width: '100%', theme: 'metro'});
+            
+            $("#jqxMenu").jqxMenu({ width: '100%', theme: 'metro'});
+            $("#jqxMenu").css('visibility', 'visible');
+            
+            $(window).resize(function(){
+            	$('#mainSplitter').css({height: contentHeight()});
+            	$('#feedExpander').css({height: feedExpanderHeight()});
+            	$('#jqxNavigationBar').css({height: feedExpanderHeight()});
+            });
+            
+            
+            var registerMenu = function(){
+            	$("#userListMenu").click(function(){
+            		console.log("click..");
+            		$.publish("viewUserListEvent", {name: "waw"});
+            	});
+            }
+            
+            registerMenu();
+            
+            require(['./controller/WorkspaceMain'], function (WorkspaceMain) {
+            	var workspaceMain = new WorkspaceMain($("#contentPanel"));
+            });
+            
+        });
+    };
+    return {
+        initialize: initialize
+    };
+});
---
+++ bpa-main/src/main/webapp/js/app/bpa-main-constant.js
@@ -0,0 +1,8 @@
+var BPA = BPA || {};
+
+BPA.Constant = BPA.Constant || {};
+
+BPA.Constant.security = {
+	usersUrl : 'service/security/users',
+	rolesUrl : 'service/security/roles'
+};
