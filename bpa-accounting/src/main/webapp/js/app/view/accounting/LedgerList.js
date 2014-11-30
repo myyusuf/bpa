34e09813d31dc20cd38eb43cc70e134ed15079d2
Yusuf <myyusuf1911@gmail.com>
2014-11-30 15:15:05 +0700

    fix ledger nested grid

--- bpa-accounting/src/main/webapp/js/app/view/accounting/LedgerList.js
+++ bpa-accounting/src/main/webapp/js/app/view/accounting/LedgerList.js
@@ -8,17 +8,17 @@ define(["jQuery", "jqxcore"], function () {
         {
             datatype: "json",
             datafields: [
-                { name: 'date', type: 'string' },
+                { name: 'accountCode', type: 'string' },
                 { name: 'accountName', type: 'string' },
-                { name: 'debit', type: 'string' },
-                { name: 'credit', type: 'string' }
+                { name: 'details'},
             ],
-            id: 'code',
+            id: 'accountCode',
             beforeprocessing: function (data) {
                 source.totalrecords = data.num;
             },
             url: url
         };
+        
         
         var dataAdapter = new $.jqx.dataAdapter(source, {
             downloadComplete: function (data, status, xhr) { 
@@ -29,6 +29,48 @@ define(["jQuery", "jqxcore"], function () {
             },
             loadError: function (xhr, status, error) { }
         });
+        
+        //-----------------------------
+        
+        var initrowdetails = function (index, parentElement, gridElement, record) {
+        	
+        	var grid = $($(parentElement).children()[0]);
+        	
+        	var temparray = [];
+        	
+        	for (var i = 0; i < record.details.length; i++) {
+        		temparray.push(record.details[i][0]);
+            }
+            
+            var ledgerDetailsSource =
+            {
+                datafields: [
+                    { name: 'date', type: 'string' },
+                    { name: 'accountName', type: 'string' },
+                    { name: 'debit', type: 'string' },
+                    { name: 'credit', type: 'string' }
+                ],
+                id: 'accountName',
+                localdata: temparray
+                
+            };
+            var nestedGridAdapter = new $.jqx.dataAdapter(ledgerDetailsSource);
+            if (grid != null) {
+                grid.jqxGrid({
+                    source: nestedGridAdapter, width: 780, height: 200,
+                    theme: 'metro',
+                    columns: [
+						{ text: 'Date', datafield: 'date', width: '25%' },
+						{ text: 'Account Name', datafield: 'accountName', width: '25%' },
+						{ text: 'Debit', datafield: 'debit', width: '25%' },
+						{ text: 'Credit', datafield: 'credit', width: '25%' }
+                   ]
+                });
+            }
+        }
+        
+        
+        //-----------------------------
         
         container.jqxGrid(
         {
@@ -43,17 +85,18 @@ define(["jQuery", "jqxcore"], function () {
             editable: false,
             selectionmode: 'multiplerows',
             columns: [
-              { text: 'Date', datafield: 'date', width: '25%' },
-              { text: 'Account Name', datafield: 'accountName', width: '25%' },
-              { text: 'Debit', datafield: 'debit', width: '25%' },
-              { text: 'Credit', datafield: 'credit', width: '25%' }
+              { text: 'Account Name', datafield: 'accountName', width: '50%' },
+              { text: 'Account Code', datafield: 'accountCode', width: '50%' }
             ],
         	theme: 'metro',
         	pagesizeoptions: ['5', '10', '20', '100'],
         	virtualmode: true,
         	rendergridrows: function () {
                 return dataAdapter.records;
-            }
+            },
+            rowdetails: true,
+            initrowdetails: initrowdetails,
+            rowdetailstemplate: { rowdetails: "<div id='grid' style='margin: 10px;'></div>", rowdetailsheight: 220, rowdetailshidden: false }
         });
         
         container.css({marginLeft: "-2px", borderTop: "0px", borderBottom: "0px", marginTop: "-1px"});
