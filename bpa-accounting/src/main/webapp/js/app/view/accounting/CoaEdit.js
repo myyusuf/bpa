69c7b283ac0c872a50b6a396ffe2e10e748645ab
Yusuf <myyusuf1911@gmail.com>
2014-12-04 17:02:51 +0700

    add tree menu items and add coa edit page

---
+++ bpa-accounting/src/main/webapp/js/app/view/accounting/CoaEdit.js
@@ -0,0 +1,15 @@
+define(["jQuery", "jqxcore", "jqxbuttons"], function () {
+	
+	var CoaEdit = function(container){
+		
+		var url = BPA.Constant.accounting.coaUrl;
+        
+        
+        container.css({marginLeft: "-2px", borderTop: "0px", borderBottom: "0px", marginTop: "-1px"});
+        
+	}
+
+    return CoaEdit;
+    
+});
+
