830b7316b3ed3f1957874864c590a0ef203a32eb
Yusuf <myyusuf1911@gmail.com>
2014-12-11 00:51:22 +0700

    apply ddd

--- bpa-base/src/main/java/id/co/oriza/bpa/base/domain/model/AssertionConcern.java
+++ bpa-base/src/main/java/id/co/oriza/bpa/base/domain/model/AssertionConcern.java
@@ -1,5 +1,21 @@
 package id.co.oriza.bpa.base.domain.model;
 
 public class AssertionConcern {
+	
+	protected AssertionConcern(){
+		super();
+	}
+	
+	protected void assertArgumentEquals(Object anObject1, Object anObject2, String aMessage){
+		if(!anObject1.equals(anObject2)){
+			throw new IllegalArgumentException(aMessage);
+		}
+	}
+	
+	protected void assertArgumentFalse(boolean aBoolean, String aMessage){
+		if(aBoolean){
+			throw new IllegalArgumentException(aMessage);
+		}
+	}
 
 }
