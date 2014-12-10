fd0aa95d4479d993c0d72411e696eb4354341f74
Yusuf <myyusuf1911@gmail.com>
2014-12-11 00:31:04 +0700

    prepare domain model for bpa accounting

--- bpa-accounting/src/main/java/id/co/oriza/bpa/acc/domain/model/Account.java
+++ bpa-accounting/src/main/java/id/co/oriza/bpa/acc/domain/model/Account.java
@@ -1,5 +1,127 @@
 package id.co.oriza.bpa.acc.domain.model;
 
-public class Account {
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "BPA_ACCOUNT")
+public class Account implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "user_seq_gen")
+	@SequenceGenerator(name = "user_seq_gen", sequenceName = "USER_ENTITY_SEQ")
+	@Column(name = "ID")
+	private Long id;
+
+	@NotEmpty
+	@Column(name = "CODE")
+	private String code;
+	
+	@NotEmpty
+	@Column(name = "NAME")
+	private String name;
+	
+	@Column(name = "DESCRIPTION")
+	private String description;
+	
+	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name = "PARENT_ID")
+	private Account parent;
+	
+	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name = "CATEGORY_ID")
+	private AccountCategory category;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Account getParent() {
+		return parent;
+	}
+
+	public void setParent(Account parent) {
+		this.parent = parent;
+	}
+	
+
+	public AccountCategory getCategory() {
+		return category;
+	}
+
+	public void setCategory(AccountCategory category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		return true;
+	}
 
 }
