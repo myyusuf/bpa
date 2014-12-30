1002ad089ff93127fa193b8cb98b305102eedaf1
Yusuf <myyusuf1911@gmail.com>
2014-12-30 22:11:27 +0700

    refactor Coa to Account

--- bpa-accounting-web/src/main/webapp/js/app/bpa-accounting-constant.js
+++ bpa-accounting-web/src/main/webapp/js/app/bpa-accounting-constant.js
@@ -5,7 +5,7 @@ BPA.Constant = BPA.Constant || {};
 BPA.Constant.accounting = {
 	defaultBalanceUrl : 'service/accounting/defaultbalances',
 	accountGroupUrl : 'service/accounting/accountgroups',
-	coaUrl : 'service/accounting/accounts',
+	accountUrl : 'service/accounting/accounts',
 	journalUrl : 'service/accounting/journal',	
 	ledgerUrl : 'service/accounting/ledger',
 	balanceSheetUrl : 'service/accounting/balancesheet',
