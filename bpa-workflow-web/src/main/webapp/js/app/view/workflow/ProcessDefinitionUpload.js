582c441b12576b3b420d4f2550cad93e5a7f755d
Yusuf <myyusuf1911@gmail.com>
2015-01-16 21:33:14 +0700

    complete bpmn upload file

--- bpa-workflow-web/src/main/webapp/js/app/view/workflow/ProcessDefinitionUpload.js
+++ bpa-workflow-web/src/main/webapp/js/app/view/workflow/ProcessDefinitionUpload.js
@@ -12,6 +12,8 @@ define(["bpaObservable", "jqxbuttons", "jqxinput", "jqxvalidator", "jqxcombobox"
 		
 		Observable.call(_self, _subscribers);
 		
+		var _randomId = BPA.Util.getRandomId("processDefinitionUpload");
+		
 		var _editWindow = $('<div id="workflowAddDiagramWindow"></div>');
 		var _windowHeader = "";
 		_windowHeader = $('<div style="height: 18px; padding: 5px; padding-top: 3px; padding-bottom: 7px;"><table><tr><td><img src="resources/images/application-dialog.png" alt="" style="margin-right: 1px" /></td><td valign="center"><span style="font-weight: bold">Add Diagram</span></td></tr></table></div>');
@@ -21,6 +23,36 @@ define(["bpaObservable", "jqxbuttons", "jqxinput", "jqxvalidator", "jqxcombobox"
 		_windowHeader.appendTo(_editWindow);
 		_windowContent.appendTo(_editWindow);
         _editWindow.appendTo(container);
+        
+		
+		var _editForm = $('<form></form>');
+		_editForm.appendTo(_windowContent);
+		var _editTable = $('<table class="edit-table"></table>');
+		_editTable.appendTo(_editForm);
+		
+		var _newRow = $('<tr></tr>');
+		_newRow.appendTo(_editTable);
+		var _codeLabel = $('<td>Code</td>');
+		_codeLabel.appendTo(_newRow);
+		var _codeInputColumn = $('<td></td>');
+		var _codeInput = $('<input type="text" class="text-input" maxlength="8" />');
+		_codeInput.attr("id", "codeInput" + _randomId);
+		
+		_codeInput.appendTo(_codeInputColumn);
+		_codeInputColumn.appendTo(_newRow);
+		
+		
+		_newRow = $('<tr></tr>');
+		_newRow.appendTo(_editTable);
+		var _fileLabel = $('<td>BPMN File</td>');
+		_fileLabel.appendTo(_newRow);
+		var _fileInputColumn = $('<td></td>');
+		var _fileInput = $('<input type="text" class="text-input" maxlength="8" />');
+		_fileInput.attr("id", "fileInput" + _randomId);
+		
+		_fileInput.appendTo(_fileInputColumn);
+		_fileInputColumn.appendTo(_newRow);
+        
         
         _editWindow.jqxWindow('resizable', true);
         _editWindow.jqxWindow('draggable', true);
