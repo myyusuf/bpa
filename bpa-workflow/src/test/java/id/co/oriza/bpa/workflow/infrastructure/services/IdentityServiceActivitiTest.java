875e2f44cd8cb356a76660d0f1ec674d83ac69fb
Yusuf <myyusuf1911@gmail.com>
2015-01-28 07:24:14 +0700

    complete workflow user

---
+++ bpa-workflow/src/test/java/id/co/oriza/bpa/workflow/infrastructure/services/IdentityServiceActivitiTest.java
@@ -0,0 +1,28 @@
+package id.co.oriza.bpa.workflow.infrastructure.services;
+
+import static org.junit.Assert.assertNotNull;
+import id.co.oriza.bpa.workflow.application.IdentityService;
+import id.co.oriza.bpa.workflow.domain.model.User;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:bpa-workflow-ctx-test.xml")
+public class IdentityServiceActivitiTest {
+	
+	@Autowired
+	private IdentityService identityService;
+	
+	@Test
+	public void testUsers(){
+		List<User> allUsers = identityService.allUsers(0, 100);
+		assertNotNull(allUsers);
+	}
+
+}
