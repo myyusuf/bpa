7ecbe0b2c986b1549fa4f031d5852e23d661b7f6
Yusuf <myyusuf1911@gmail.com>
2015-01-30 06:41:17 +0700

    complete workflow group

---
+++ bpa-workflow-web/src/main/webapp/js/app/composer/workflow/identity/GroupComposer.js
@@ -0,0 +1,125 @@
+define(["notificationWindow", "view/workflow/identity/GroupList", "view/workflow/identity/GroupEdit", "jqxnotification"], function (NotificationWindow, GroupList, GroupEdit) {
+	
+	var GroupComposer = function(container){
+		
+		var _self = this;
+		
+		var _groupListUrl = BPA.Constant.workflow.identity.groupUrl;
+		
+		var _successNotification = $('<div>Data successfully saved</div>').jqxNotification({
+            width: 250, position: "top-right", opacity: 0.9,
+            autoOpen: false, animationOpenDelay: 800, autoClose: true, autoCloseDelay: 3000, template: "info"
+        });
+		var _successDeleteNotification = $('<div>Data successfully deleted</div>').jqxNotification({
+            width: 250, position: "top-right", opacity: 0.9,
+            autoOpen: false, animationOpenDelay: 800, autoClose: true, autoCloseDelay: 3000, template: "info"
+        });
+			
+		var _options = {};
+		_options.url = _groupListUrl;
+		
+		var _groupList = new GroupList(container, _options);
+		
+		GroupComposer.prototype.buildOnUpdateGroup = function(groupList, groupEdit){
+			var _onUpdateGroup = function(updatedGroup){
+				
+				var _requestType = "PUT";
+				
+				var _onSuccess = function(result){// Depends on new _groupEdit instance
+					groupEdit.close();//new _groupEdit instance
+					groupList.refreshGrid();
+					_successNotification.jqxNotification("open");
+				}
+				
+				var _onError = function(status, error){
+					var _errorWindow = new NotificationWindow(container, {title:'Error Saving Group', 
+						content: 'Error status : '+ status + '<br>Error message : '+ error, type: 'error'});
+				}
+				
+				_sendData(updatedGroup, _requestType, _onSuccess, _onError);
+			}
+			
+			return _onUpdateGroup;
+		}
+		
+		var _onEditRow = function(editedGroup){
+			
+			//Consider always new instance
+			var _groupEdit = new GroupEdit(container, {editedGroup: editedGroup});
+			
+			var _onUpdateGroup = _self.buildOnUpdateGroup(_groupList, _groupEdit);
+			_groupEdit.subscribe(_onUpdateGroup, "updategroup");
+			
+			//Because this listener always depends on _groupEdit new instance, it must also always defined again
+			var _onAddNewGroup = function(newGroup){
+				
+				var _requestType = "POST";
+				
+				var _onSuccess = function(result){// Depends on new _groupEdit instance
+					_groupEdit.close();//new _groupEdit instance
+					_groupList.refreshGrid();
+					_successNotification.jqxNotification("open");
+				}
+				
+				var _onError = function(status, error){
+					var _errorWindow = new NotificationWindow(container, {title:'Error Saving Group', 
+						content: 'Error status : '+ status + '<br>Error message : '+ error, type: 'error'});
+				}
+				
+				_sendData(newGroup, _requestType, _onSuccess, _onError);
+			}
+			_groupEdit.subscribe(_onAddNewGroup, "addnewgroup");
+
+			_groupEdit.open();
+		};
+		_groupList.subscribe(_onEditRow, "editrow");
+		
+		var _onDeleteRow = function(deletedGroup){
+			
+			var _onOk = function(){
+				var _requestType = "DELETE";
+				var _onSuccess = function(result){
+					_groupList.refreshGrid();
+					_successDeleteNotification.jqxNotification("open");
+				}
+				
+				var _onError = function(status, error){
+					var _errorWindow = new NotificationWindow(container, {title:'Error Deleting Group', 
+						content: 'Error status : '+ status + '<br>Error message : '+ error, type: 'error'});
+				}
+				
+				_sendData(deletedGroup, _requestType, _onSuccess, _onError);
+			}
+			
+			var _deleteConfirmationWindow = new NotificationWindow(container, {title:'Delete Group', 
+			content: "Are you sure want to delete this group : " + deletedGroup.code + " (" + deletedGroup.firstName + ") ?", type: 'info', onOk: _onOk});
+			
+			
+		};
+		_groupList.subscribe(_onDeleteRow, "deleterow");
+		
+		var _sendData = function(data, requestType, onSuccess, onError){
+			$.ajax({
+			    url: _groupListUrl,
+			    type: requestType,
+			    data: JSON.stringify(data),
+			    beforeSend: function(xhr) {
+		            xhr.setRequestHeader("Accept", "application/json");
+		            xhr.setRequestHeader("Content-Type", "application/json");
+		        },
+			    success: function(result) {
+			    	onSuccess(result);
+			    },
+			    error: function(jqXHR, status, error){
+			    	onError(jqXHR.status, error);
+			    }
+			});
+		};
+		
+		container.css({marginLeft: "-2px", borderTop: "0px", borderBottom: "0px", marginTop: "-1px"});
+        
+	}
+	
+    return GroupComposer;
+    
+});
---
+++ bpa-workflow-web/src/main/webapp/js/app/view/workflow/identity/GroupList.js
@@ -0,0 +1,185 @@
+define(["bpaObservable", "jQuery", "jqxcore", "jqxbuttons", "jqxdata", "jqxinput", "jqxmenu",
+        "jqxgrid", "jqxgrid.pager", "jqxgrid.sort", "jqxgrid.edit", "jqxgrid.selection"
+        ], function (Observable) {
+	
+	var GroupList = function(container, options){
+		
+		var _self = this;
+		
+		var _options = options || {};
+		
+		var _url = BPA.Constant.workflow.identity.groupsUrl;
+		
+		var _subscribers = {
+			any:[]
+		};
+		
+		Observable.call(_self, _subscribers);
+		
+        var _source =
+        {
+            datatype: "json",
+            datafields: [
+                { name: 'groupId', type: 'string' },
+                { name: 'firstName', type: 'string' },
+                { name: 'lastName', type: 'string' },
+                { name: 'email', type: 'string' }
+            ],
+            id: 'groupId',
+            beforeprocessing: function (data) {
+                _source.totalrecords = data.num;
+            },
+            url: _url
+        };
+        
+        var _dataAdapter = new $.jqx.dataAdapter(_source, {
+            downloadComplete: function (data, status, xhr) { 
+            },
+            loadComplete: function (data) { 
+            	//console.log('data : ' + data);
+            	
+            },
+            loadError: function (xhr, status, error) { }
+        });
+        
+        var _groupListGrid = container.jqxGrid(
+        {
+            width: '100%',
+            height: '100%',
+            source: _dataAdapter,                
+            pageable: true,
+            autoheight: false,
+            sortable: true,
+            altrows: true,
+            enabletooltips: true,
+            editable: false,
+            selectionmode: 'singlerow',
+            columns: [
+              { text: 'Group Id', datafield: 'groupId', width: '25%' },
+              { text: 'First Name', datafield: 'firstName', width: '25%' },
+              { text: 'Last Name', datafield: 'lastName', width: '25%' },
+              { text: 'Email', datafield: 'email', width: '25%' }
+            ],
+        	theme: 'metro',
+        	pagesizeoptions: ['5', '10', '20', '100'],
+        	virtualmode: true,
+        	rendergridrows: function () {
+                return _dataAdapter.records;
+            },
+            showtoolbar: true,
+            toolbarheight: 40,
+            rendertoolbar: function(toolbar)
+            {
+            	toolbar.empty();
+            	
+                var _searchContainer = $("<div style='float: left; margin: 5px; text-align: right;'></div>");
+                var _searchTable = $('<table></table>');
+                _searchTable.appendTo(_searchContainer);
+        		
+        		var _newRow = $('<tr></tr>');
+        		_newRow.appendTo(_searchTable);
+        		var _newColumn = $('<td></td>');
+        		_newColumn.appendTo(_newRow);
+        		var _addButton = $('<div style="margin-left: 2px;">New Group</div>');
+        		_addButton.appendTo(_newColumn);
+        		
+                toolbar.append(_searchContainer);
+                
+                _addButton.jqxButton({ width: '116', height: '16', theme: 'metro' });
+                           
+                _addButton.click(function(event){
+                	_showEditPage();
+                });
+            },
+        });
+        
+        _groupListGrid.on('rowdoubleclick', function (event){ 
+        	var _args = event.args, _rowindex = _args.rowindex;
+        	var _rowData = _groupListGrid.jqxGrid('getrowdata', _rowindex);
+            _showEditPage(_rowData);
+        });
+        
+        var _gridContextMenu = $('<div><ul><li data-menukey="add">Add New</li><li data-menukey="edit">Edit</li><li data-menukey="delete">Delete</li></ul></div>');
+        _gridContextMenu.jqxMenu({width: '120px', autoOpenPopup: false, mode: 'popup', theme: 'metro'});
+        _gridContextMenu.on('itemclick', function (event){
+        	
+        	var _menuKey = $(event.target).data("menukey");
+        	
+        	var _rowData = "";
+        	
+        	var _rowIndex = _groupListGrid.jqxGrid('getselectedrowindex');
+        	var _rowData = _groupListGrid.jqxGrid('getrowdata', _rowIndex);
+        	
+	 		if("add" == _menuKey){
+	 			_showEditPage();
+	 		}else if("edit" == _menuKey){
+	 			_showEditPage(_rowData);
+	 		}else if("delete" == _menuKey){
+	 			_deleteRow(_rowData);
+	 		}
+        });
+        
+        _groupListGrid.on('rowclick', function (event) {
+        	
+        	var _args = event.args, _clickEvent = _args.originalEvent, _rowIndex = args.rowindex;
+        	
+        	var _rightClick = _isRightClick(_clickEvent);
+            if (_rightClick) {
+            	if(_groupListGrid.jqxGrid('getselectedrowindex') === -1){
+            		_groupListGrid.jqxGrid('selectrow', _rowIndex);
+            	}
+            	
+                var _scrollTop = $(window).scrollTop();
+                var _scrollLeft = $(window).scrollLeft();
+                _gridContextMenu.jqxMenu('open', parseInt(_clickEvent.clientX) + 5 + _scrollLeft, parseInt(_clickEvent.clientY) + 5 + _scrollTop);
+                return false;
+            }else{
+            	_gridContextMenu.jqxMenu('close');
+            }
+        });
+        
+        _groupListGrid.on('contextmenu', function (e) {
+            return false;
+        });
+        
+        var _showEditPage = function(rowData){
+        	Observable.prototype.publish.call(_self, _getGroupFromRowData(rowData), "editrow");
+        }
+        
+        var _deleteRow = function(rowData){
+        	Observable.prototype.publish.call(_self, _getGroupFromRowData(rowData), "deleterow");
+        }
+        
+        var _getGroupFromRowData = function(rowData){
+        	var _group = {};
+        	
+        	if(rowData){
+        		_group.groupId = rowData.groupId;
+            	_group.firstName = rowData.firstName;
+            	_group.lastName = rowData.lastName;
+            	_group.email = rowData.email;
+        	}
+        	
+        	return _group;
+        }
+        
+        this.refreshGrid = function(){
+        	_groupListGrid.jqxGrid('updatebounddata');
+        }
+        
+        var _isRightClick = function(event) {
+            var _rightclick;
+            if (!event) var event = window.event;
+            if (event.which) _rightclick = (event.which == 3);
+            else if (event.button) _rightclick = (event.button == 2);
+            return _rightclick;
+        }
+        
+	}
+	
+	inheritPrototype(GroupList, Observable);
+
+    return GroupList;
+    
+});
+
