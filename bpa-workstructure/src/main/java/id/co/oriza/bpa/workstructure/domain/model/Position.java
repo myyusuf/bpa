a1e21a8a906b3b7ba1682e8a3340647acef0dda4
Yusuf <myyusuf1911@gmail.com>
2015-03-12 07:21:32 +0700

    bpa-workstructure position

--- bpa-workstructure/src/main/java/id/co/oriza/bpa/workstructure/domain/model/Position.java
+++ bpa-workstructure/src/main/java/id/co/oriza/bpa/workstructure/domain/model/Position.java
@@ -1,5 +1,47 @@
 package id.co.oriza.bpa.workstructure.domain.model;
 
-public class Position {
+import id.co.oriza.bpa.base.domain.model.IdentifiedValueObject;
+
+public class Position extends IdentifiedValueObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String code;
+	private String name;
+	
+	protected Position() {
+		super();
+	}
+
+	public Position(String aCode, String aName) {
+		this();
+		this.setCode(aCode);
+		this.setName(aName);
+	}
+
+	public String code() {
+		return code;
+	}
+
+	public String name() {
+		return name;
+	}
+
+	protected void setCode(String aCode) {
+		this.assertArgumentNotNull(aCode, "The code is required.");
+		this.code = aCode;
+	}
+
+	protected void setName(String aName) {
+		this.assertArgumentNotNull(aName, "The name is required.");
+		this.name = aName;
+	}
+	
+	public void changeName(String aName) {
+		this.setName(aName);
+	}
 
 }
