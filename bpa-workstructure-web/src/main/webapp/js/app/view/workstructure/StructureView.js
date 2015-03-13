68b4ac8b4e1909c18abe677699b9102750b1c713
Yusuf <myyusuf1911@gmail.com>
2015-03-13 08:40:49 +0700

    bpa-workstructure Structure

--- bpa-workstructure-web/src/main/webapp/js/app/view/workstructure/StructureView.js
+++ bpa-workstructure-web/src/main/webapp/js/app/view/workstructure/StructureView.js
@@ -6,8 +6,6 @@ define(["bpaObservable", "component/base/SimpleListGrid", "jQuery", "jqxcore", "
 		
 		var _self = this;
 		
-		var _options = {};
-		
 		var _subscribers = {
 			any:[]
 		};
@@ -23,16 +21,18 @@ define(["bpaObservable", "component/base/SimpleListGrid", "jQuery", "jqxcore", "
         _buttons.push(new primitives.orgdiagram.ButtonConfig("add", "ui-icon-person", "Add"));
         
 		var _maximumId = 0;
-		var id = ++_maximumId;
-		console.log('id --> ' + id);
+		
+		var _id = ++_maximumId;
+		console.log('_maximumId --> ' + _maximumId);
 		
 		var _item = 
             new primitives.orgdiagram.ItemConfig({
-                id: id,
-                parent: '',
+                id: _id,
+                parent: null,
                 title: 'firstName',
                 description: 'positionName',
-                context: {data: 'test'}
+                context: {data: 'test'},
+                image: "service/workstructure/employee/loadimage/"
             });
 		
 		_items.push(_item);
@@ -44,12 +44,63 @@ define(["bpaObservable", "component/base/SimpleListGrid", "jQuery", "jqxcore", "
 		_options.hasButtons = primitives.common.Enabled.Auto;
 		_options.leavesPlacementType = primitives.orgdiagram.ChildrenPlacementType.Matrix;
 		
-		var _chartContainer = $('<div id="orgchart" style="height: 500px">dddd</div>');
+		var _chartContainer = $('<div id="orgchart" style="height: 500px;">dd</div>');
 		_chartContainer.appendTo(container);
 		
 		$('#orgchart').orgDiagram(_options);
 		$('#orgchart').orgDiagram("update", primitives.orgdiagram.UpdateMode.Refresh);
+		
+		var _newItem = new primitives.orgdiagram.ItemConfig({
+            id: ++_maximumId,
+            parent: _id,
+            title: 'employeeName',
+            description: 'positionName',
+            context: {data: 'test'},
+            image: "service/workstructure/employee/loadimage/"
+        });
+
+    	_items.push(_newItem);
+        $("#orgchart").orgDiagram({
+            items: _items,
+            cursorItem: _newItem.id
+        });
+        $('#orgchart').orgDiagram("update", primitives.orgdiagram.UpdateMode.Refresh);
 	        
+		
+		/*var _chartContainer = $('<div id="orgchart" style="height: 500px; background-color: silver;">dd</div>');
+		_chartContainer.appendTo(container);
+		
+		var options = new primitives.orgdiagram.Config();
+
+        var items = [
+            new primitives.orgdiagram.ItemConfig({
+                id: 0,
+                parent: null,
+                title: "Scott Aasrud",
+                description: "VP, Public Sector",
+                image: "demo/images/photos/a.png"
+            }),
+            new primitives.orgdiagram.ItemConfig({
+                id: 1,
+                parent: 0,
+                title: "Ted Lucas",
+                description: "VP, Human Resources",
+                image: "demo/images/photos/b.png"
+            }),
+            new primitives.orgdiagram.ItemConfig({
+                id: 2,
+                parent: 0,
+                title: "Joao Stuger",
+                description: "Business Solutions, US",
+                image: "demo/images/photos/c.png"
+            })
+        ];
+
+        options.items = items;
+        options.cursorItem = 0;
+        options.hasSelectorCheckbox = primitives.common.Enabled.True;
+
+        jQuery("#orgchart").orgDiagram(options);*/
 	}
 	
 	inheritPrototype(StructureView, Observable);
