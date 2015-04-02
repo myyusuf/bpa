2850be16453ce3c490eefa0b958ec65591ea148f
Yusuf <myyusuf1911@gmail.com>
2015-04-02 09:10:37 +0700

    bpa-accounting journal

--- bpa-accounting-web/src/main/webapp/js/app/view/accounting/JournalList.js
+++ bpa-accounting-web/src/main/webapp/js/app/view/accounting/JournalList.js
@@ -1,71 +1,87 @@
-define(["jQuery", "jqxcore"], function () {
+define(["bpaObservable", "component/base/SimpleListGrid", "jQuery", "jqxcore", "jqxbuttons", "jqxdata", "jqxinput", "jqxmenu",
+        "jqxgrid", "jqxgrid.pager", "jqxgrid.sort", "jqxgrid.edit", "jqxgrid.selection"
+        ], function (Observable, SimpleListGrid) {
 	
-	var JournalList = function(container){
+	var JournalList = function(container, url){
 		
-		var url = BPA.Constant.accounting.journalUrl;
-        
-        var source =
-        {
-            datatype: "json",
-            datafields: [
-                { name: 'date', type: 'string' },
-                { name: 'accountName', type: 'string' },
-                { name: 'debit', type: 'string' },
-                { name: 'credit', type: 'string' }
-            ],
-            id: 'code',
-            beforeprocessing: function (data) {
-                source.totalrecords = data.num;
-            },
-            url: url
-        };
-        
-        var dataAdapter = new $.jqx.dataAdapter(source, {
-            downloadComplete: function (data, status, xhr) { 
-            },
-            loadComplete: function (data) { 
-            	//console.log('data : ' + data);
-            	
-            },
-            loadError: function (xhr, status, error) { }
+		var _self = this;
+		
+		var _options = {};
+		
+		var _subscribers = {
+			any:[]
+		};
+		
+		Observable.call(_self, _subscribers);
+		
+		_options.dataFields = [
+		                       { name: 'journalId', type: 'string' },
+		                       { name: 'date', type: 'string' },
+		                       { name: 'accountName', type: 'string' },
+		                       { name: 'debit', type: 'string' },
+		                       { name: 'credit', type: 'string' }
+		                   ];
+		_options.dataFieldId = "journalId";
+		
+		_options.url = url || BPA.Constant.workflow.identity.journalsUrl;
+		
+		_options.columns = [
+		                   { text: 'Date', datafield: 'date', width: '25%' },
+		                   { text: 'Account Name', datafield: 'name', width: '25%' },
+		                   { text: 'Debit', datafield: 'date', width: '25%' },
+		                   { text: 'Credit', datafield: 'name', width: '25%' }
+		                 ];
+		
+		var _addButton = $('<div style="margin-left: 2px;">New Journal</div>');
+		_addButton.jqxButton({ width: '116', height: '16', theme: 'metro' });
+		_addButton.click(function(event){
+			Observable.prototype.publish.call(_self, {}, "onAddJournal");
         });
+		
+		_options.toolbarButtons = [_addButton];
+		
+		var _simpleListGrid = new SimpleListGrid(container, _options);
+		
+		var _onContextMenuClick = function(commandObject){
+			var _command = commandObject.command;
+			var _rowData = commandObject.rowData;
+			console.log(_command);
+			
+			var _eventName = "";
+			if(_command == "add"){
+				_eventName = "onAddJournal";
+			}else if(_command == "edit"){
+				_eventName = "onEditJournal";
+			}else if(_command == "delete"){
+				_eventName = "onDeleteJournal";
+			}
+			Observable.prototype.publish.call(_self, _getJournalFromRowData(_rowData), _eventName);
+		}
+		_simpleListGrid.subscribe(_onContextMenuClick, "onContextMenuClick");
+		
+		var _getJournalFromRowData = function(rowData){
+        	var _journal = {};
+        	
+        	if(rowData){
+        		_journal.id = rowData.id;
+            	_journal.name = rowData.name;
+        	}
+        	
+        	return _journal;
+        }
+		
+        this.refreshGrid = function(){
+        	_simpleListGrid.refreshGrid();
+        }
         
-        var journalListGrid = container.jqxGrid(
-        {
-            width: '100%',
-            height: '100%',
-            source: dataAdapter,                
-            pageable: true,
-            autoheight: false,
-            sortable: true,
-            altrows: true,
-            enabletooltips: true,
-            editable: false,
-            selectionmode: 'multiplerows',
-            columns: [
-              { text: 'Date', datafield: 'date', width: '25%' },
-              { text: 'Account Name', datafield: 'accountName', width: '25%' },
-              { text: 'Debit', datafield: 'debit', width: '25%' },
-              { text: 'Credit', datafield: 'credit', width: '25%' }
-            ],
-        	theme: 'metro',
-        	pagesizeoptions: ['5', '10', '20', '100'],
-        	virtualmode: true,
-        	rendergridrows: function () {
-                return dataAdapter.records;
-            }
-        });
+        this.getComponent = function(){
+        	return _simpleListGrid.getComponent();
+        }
         
-        journalListGrid.on('rowdoubleclick', function (event){ 
-        	console.log('test');
-//        	require(['./view/accounting/CoaEdit'], function (CoaEdit) {
-//            	var coaEdit = new CoaEdit(container);
-//            });   
-        });
-        
-        container.css({marginLeft: "-2px", borderTop: "0px", borderBottom: "0px", marginTop: "-1px"});
         
 	}
+	
+	inheritPrototype(JournalList, Observable);
 
     return JournalList;
     
