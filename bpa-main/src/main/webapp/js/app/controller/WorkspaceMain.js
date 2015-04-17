5035dd7df19708c2b2fbd36238b8ac4f0d051d39
Yusuf <myyusuf1911@gmail.com>
2015-04-18 00:59:35 +0700

    bpa-main integration with fix tab selection method

--- bpa-main/src/main/webapp/js/app/controller/WorkspaceMain.js
+++ bpa-main/src/main/webapp/js/app/controller/WorkspaceMain.js
@@ -2,23 +2,22 @@ define(["jQuery", "jqxcore"], function () {
 	
 	var WorkspaceMain = function(container){
 		
-		var _tabPosition = {};
-		
 		var _addTab = function(tabId, tabCaption, onContentLoad){
 			var _tabsCount = 0;
+			var _positionIndex = -1;
 			
-			var _positionIndex = _tabPosition[tabId];
-			if(_positionIndex){
+			for(_index = 0; _index < tabs.jqxTabs('length'); _index++){
+				if(tabs.jqxTabs('getTitleAt', _index) == tabCaption){
+					_positionIndex = _index;
+				}
+			}
+			
+			if(_positionIndex > -1){
 				tabs.jqxTabs('select', _positionIndex);
 			}else{
 				_positionIndex = tabs.jqxTabs('length');
 				tabs.jqxTabs('addLast', tabCaption , tabId + "_content");
 				tabs.jqxTabs('setContentAt', _positionIndex , '<div id="' + tabId + '" >Loading content...</div>');
-				_tabPosition[tabId] = _positionIndex;
-				
-				tabs.on('removed', function (event) { 
-					delete _tabPosition[tabId];
-				}); 
 				
 				var _parentContainer = $('#' + tabId).parent();
 				var _children = _parentContainer.children();
@@ -35,7 +34,7 @@ define(["jQuery", "jqxcore"], function () {
 		var tabs = $('<div id="tabs"><ul><li>Dashboard</li></ul><div></div></div>');
 		
 		tabs.appendTo(container);
-		tabs.jqxTabs({ width: '100%', height: '100%', position: 'top', showCloseButtons: true, scrollPosition: 'both', theme: 'metro'});
+		tabs.jqxTabs({ width: '100%', height: '100%', position: 'top', showCloseButtons: true, scrollPosition: 'both', selectionTracker: true, theme: 'metro'});
 		tabs.css({marginLeft: "-1px", marginTop: "0px", borderTop: "0px"});
 		
 		
@@ -75,29 +74,24 @@ define(["jQuery", "jqxcore"], function () {
 		});
 		
 		$.subscribe("viewAccountGroupListEvent", function(e, data){
-			var tabsCount = tabs.jqxTabs('length');
+			var _onContentLoad = function(container){
+				require(['./composer/accounting/AccountGroupComposer'], function (AccountGroupComposer) {
+	            	var accountGroupComposer = new AccountGroupComposer(container);
+	            });
+			}
 			
-			tabs.jqxTabs('addLast', 'Account Group' , 'accountGroupListGrid');
-			tabs.jqxTabs('setContentAt', tabsCount , '<div id="accountGroupListGrid">Account Group Grid : [Loading]</div>');
-			
-			require(['./composer/accounting/AccountGroupComposer'], function (AccountGroupComposer) {
-				
-				var parentContainer = $('#accountGroupListGrid').parent();
-            	var accountGroupComposer = new AccountGroupComposer(parentContainer);
-            });
+			_addTab("accounting_accountgroup", "Account Group", _onContentLoad);
 			
 		});
 		$.subscribe("viewCoaListEvent", function(e, data){
-			var tabsCount = tabs.jqxTabs('length');
+			var _onContentLoad = function(container){
+				require(['./composer/accounting/AccountComposer'], function (AccountComposer) {
+	            	var accountComposer = new AccountComposer(container);
+	            });
+			}
 			
-			tabs.jqxTabs('addLast', 'Chart of Account' , 'coaListGrid');
-			tabs.jqxTabs('setContentAt', tabsCount , '<div id="coaListGrid">Coa Grid : [Loading]</div>');
+			_addTab("accounting_account", "Account", _onContentLoad);
 			
-			require(['./composer/accounting/AccountComposer'], function (AccountComposer) {
-				
-				var parentContainer = $('#coaListGrid').parent();
-            	var accountComposer = new AccountComposer(parentContainer);
-            });
 			
 		});
 		$.subscribe("viewLedgerListEvent", function(e, data){
